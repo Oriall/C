1de11f8d2d150bb05232b122458f9b3d966b1aee
Orias <vokhanhtoannghithu@gmail.com>
2026-05-22 17:05:25 +0700

    updae

--- Basic C/b1.c
+++ Basic C/b1.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #define ll long long
-int main() {
-    ll n,a[100002],tong=0;
-    scanf("%lld", &n);
-    if (n<1 || n>100) {
-        printf("INVALID");
-        return 0;
-    } else {
-       
-    for (ll i = 1; i <= n; i++) {
-        scanf("%lld", &a[i]);
-        if (a[i] % 3 == 0) {
-            tong+=a[i];
+void sort(int a[], int n) {
+    for (int i=1;i<n;i++){
+        for (int j=i+1;j<=n;j++){
+            if (a[i]>a[j]){
+                int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
         }
     }
-    printf("%lld", tong);
+}
+int main() {
+    int a[10002],nn,n;
+    scanf("%d", &n);
+    for (int i=1;i<=n;i++){
+        scanf("%d", &a[i]);
+    }
+    sort(a,n);
+    for (int i=1;i<=n;i++){
+        printf("%d ", a[i]);
+    }
 }
     
-}
